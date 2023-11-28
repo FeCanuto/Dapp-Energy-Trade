bbcc8630d542243b51e47200cea9e466b04ba38f
FeCanuto <felipee.canuto@hotmail.com>
2023-11-28 20:01:28 -0300

    Adicionado método async RequestAccount por questões de segurança involvendo a interação com o metamask

--- Frontend/src/app/ui/data-table/data-table.component.ts
+++ Frontend/src/app/ui/data-table/data-table.component.ts
@@ -3,7 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import Web3 from 'web3';
 
 const web3 = new Web3(Web3.givenProvider);
-const contractAddress = "0xF7955416c2343bA69450873D3d20e65D85c17b67"; // Replace with your contract address
+const contractAddress = "0xDfA55BfDC43D18b7F4f0f6448C3e9f523b3084bd"; // Replace with your contract address
 const tokenAbi = require("../../../../../Blockchain/build/contracts/EnergyTrade.json");
 const contractInstance = new web3.eth.Contract(tokenAbi.abi, contractAddress);
 
@@ -18,7 +18,7 @@ const contractInstance = new web3.eth.Contract(tokenAbi.abi, contractAddress);
 export class DataTableComponent implements OnInit {
   prosumerList: any[];
 
-  displayedColumns = ['prosumerAddress','uc', 'energiaConsumida', 'energiaInjetada', 'payProsumer'];
+  displayedColumns = ['prosumerAddress', 'uc', 'energiaConsumida', 'energiaInjetada', 'payProsumer'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -35,11 +35,24 @@ export class DataTableComponent implements OnInit {
   }
 
   //Pagar prosumer que tem créditos de energia a serem recebidos
-  payProsumer(address: any): void {
-    contractInstance.methods.payProsumerOwner(address).call();
+  async payProsumer(address: any): Promise<void> {
+    const accountAddress = await this.requestAccount();
+    await contractInstance.methods.payProsumerOwner(address).send({ from: accountAddress });
+  }
+
+  async requestAccount() {
+    try {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      return accounts[0];
+    } catch (error) {
+      console.error('User denied account access');
+      return null;
+    }
   }
 
   truncateText(text: string, maxLength: number): string {
-    return text.length > maxLength ? text.substring(0,10) + '...' + text.substring(maxLength, 42) : text;
+    return text.length > maxLength ? text.substring(0, 10) + '...' + text.substring(maxLength, 42) : text;
   }
 }
